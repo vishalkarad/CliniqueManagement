b9f33d1625910f05ca18ae306d7dd698401546d1
vishalkarad <vishalkarad777@gmail.com>
2020-05-07 21:49:15 +0530

    ADDED : add new method to read file

--- src/main/java/com/bridgelabz/services/CliniqueManagementMain.java
+++ src/main/java/com/bridgelabz/services/CliniqueManagementMain.java
@@ -1,19 +1,23 @@
 package com.bridgelabz.services;
 
+import com.bridgelabz.exception.CliniqueException;
 import com.bridgelabz.interfaces.CliniqueInterface;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CliniiqueManagementMain implements CliniqueInterface {
+public class CliniqueManagementMain implements CliniqueInterface {
 
     // Variables
     String filePath;
     File file;
+    ObjectMapper mapper = new ObjectMapper();
 
     // Constructor
-    public CliniiqueManagementMain(String filePath) {
+    public CliniqueManagementMain(String filePath) {
         this.filePath = filePath;
         this.file = new File(filePath);
     }
@@ -28,6 +32,18 @@ public class CliniiqueManagementMain implements CliniqueInterface {
             list.add(object);
             saveRecord();
             return "Add Records Successfully";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private <T> List<T> readFile() {
+        try {
+            if (file.length() == 0)
+                throw new CliniqueException(CliniqueException.MyException.FILE_EMPTY, "File is Empty");
+            return mapper.readValue(file, new TypeReference<ArrayList<T>>() {
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }

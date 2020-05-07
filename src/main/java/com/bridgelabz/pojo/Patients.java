52a43f7ed541e424092731244d36567ac409ad7d
vishalkarad <vishalkarad777@gmail.com>
2020-05-07 21:23:06 +0530

    ADDED : add new pojo class patients

---
+++ src/main/java/com/bridgelabz/pojo/Patients.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.pojo;
+
+public class Patients {
+    String patients_Id;
+    String patients_Name;
+    String patients_MobileNumber;
+    int patient_Age;
+
+    public Patients(String patients_Id, String patients_Name, String patients_MobileNumber, int patient_Age) {
+        this.patients_Id = patients_Id;
+        this.patients_Name = patients_Name;
+        this.patients_MobileNumber = patients_MobileNumber;
+        this.patient_Age = patient_Age;
+    }
+
+    // Getter methods
+    public String getPatients_Id() {
+        return patients_Id;
+    }
+
+    public String getPatients_Name() {
+        return patients_Name;
+    }
+
+    public String getPatients_MobileNumber() {
+        return patients_MobileNumber;
+    }
+
+    public int getPatient_Age() {
+        return patient_Age;
+    }
+}

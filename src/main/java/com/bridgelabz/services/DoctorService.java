e9d40a6a76973263914299e3c3807927cd73952b
vishalkarad <vishalkarad777@gmail.com>
2020-05-08 12:04:22 +0530

    ADDED : add new method to check doctor patient

--- src/main/java/com/bridgelabz/services/DoctorService.java
+++ src/main/java/com/bridgelabz/services/DoctorService.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.services;
 
+import com.bridgelabz.pojo.AppointMent;
 import com.bridgelabz.pojo.Doctor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DoctorService extends CliniqueManagementMain {
+
 
     // constructor
     public DoctorService(String filePath) {
@@ -10,7 +16,7 @@ public class DoctorService extends CliniqueManagementMain {
     }
 
     // Add doctor record
-    public String addDoctorEntry(Doctor doctor){
+    public String addDoctorEntry(Doctor doctor) {
         return super.addRecord(doctor);
     }
 
@@ -20,7 +26,20 @@ public class DoctorService extends CliniqueManagementMain {
     }
 
     // list of all doctor records
-    public String listOfAllDoctorRecords(){
+    public String listOfAllDoctorRecords() {
         return super.listOfAllRecords();
     }
+
+     // Doctor patient report
+     public int doctorPatientReport(String doctorId) {
+         AtomicInteger noOfRecord = new AtomicInteger();
+         new AppointmentService().readFile().stream().forEach(value -> {
+             if (value.getDoctor_Id().compareTo(doctorId)==0) {
+                 searchDoctorEntry(value.getPatient_Id());
+                 noOfRecord.getAndIncrement();
+             }
+         });
+         return noOfRecord.get();
+     }
+
 }

76374f3a62c12f2bde95d8d8f5e1d21a379b19cd
vishalkarad <vishalkarad777@gmail.com>
2020-05-08 12:07:46 +0530

    ADDED : add patientId getter method

--- src/main/java/com/bridgelabz/pojo/AppointMent.java
+++ src/main/java/com/bridgelabz/pojo/AppointMent.java
@@ -2,33 +2,41 @@ package com.bridgelabz.pojo;
 
 public class AppointMent {
 
-    private int appointMent_Id;
-    private String doctor_id;
+    private int appointMent_Number;
+    private String doctor_Id;
     private String appointment_Date;
+    private String patient_Id;
 
-    public AppointMent(int appointMent_Id, String doctor_id, String appointment_Date) {
-        this.appointMent_Id = appointMent_Id;
-        this.doctor_id = doctor_id;
+    public AppointMent(int appointMent_Number, String doctor_Id, String appointment_Date, String patient_Id) {
+        this.appointMent_Number = appointMent_Number;
+        this.doctor_Id = doctor_Id;
         this.appointment_Date = appointment_Date;
+        this.patient_Id = patient_Id;
     }
 
     public AppointMent() {
     }
 
     // Getter methods
-    public int getAppointMent_Id() {
-        return appointMent_Id;
+
+
+    public int getAppointMent_Number() {
+        return appointMent_Number;
     }
 
-    public String getDoctor_id() {
-        return doctor_id;
+    public String getDoctor_Id() {
+        return doctor_Id;
     }
 
     public String getAppointment_Date() {
         return appointment_Date;
     }
 
-    public void setAppointMent_Id(int appointMent_Id) {
-        this.appointMent_Id = appointMent_Id;
+    public String getPatient_Id() {
+        return patient_Id;
+    }
+
+    public void setAppointMent_Number(int appointMent_Number) {
+        this.appointMent_Number = appointMent_Number;
     }
 }
